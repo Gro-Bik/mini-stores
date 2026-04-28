ceca4d604ccd7a2514795d385a7769160b913a0d
Boris <108396167+Yashertasher@users.noreply.github.com>
2026-04-28 16:21:25 +0300

    task 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,25 @@ export default function App() {
   return (
     <>
       <h1 className="header">MiniStore</h1>
-      <p>Первый экран</p>
+      <h2>Приложения</h2>
+
+      <div className="Textblock">
+        <h3>Приложение 1</h3>
+        <p>супер пупер мое приложение</p>
+      </div>
+
+      <div className="Textblock">
+        <h3>Приложение 2</h3>
+        <p>супер пупер мое приложение</p>
+      </div>
+
+      <div className="Textblock">
+        <h3>Приложение 2</h3>
+        <p>супер пупер мое приложение</p>
+      </div>
+
+      <p>О нашем сайте.</p>
+      <p>asass</p>
     </>
   );
 }
