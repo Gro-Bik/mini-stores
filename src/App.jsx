62ebbdd3488d4fa5313d2315bc9da168bbf7eef5
Boris <108396167+Yashertasher@users.noreply.github.com>
2026-05-07 12:15:11 +0300

    semantick upgrades & general fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 import styles from './App.module.css'
 import {AppCard} from './AppCard'
+import {apps} from './variables'
+import './style_variables.css'
 
 
 export default function App() {
   return (
     <>
-    <header id='header'>
-      <h2  className={styles.header}><img width="50" src="../IMG/Micke_Icon.png"></img>Micke Shop</h2>
-      </header>
+    <header>
+      <img width="50" src="../IMG/Micke_Icon.png"></img>
+      <h2>Micke Shop</h2>
+    </header>
       
       <main>
-        {[
-          {title: 'Футболка Кага', text: 'Футболка с персонажем Кага из Kancolle', date: '27.04', price: '1500₽'}, 
-          {title: 'Кепка Эллиса', text: 'Кепка Эллиса из Left 4 Dead 2', date: '28.04', price: '975₽'}, 
-          {title: 'Фигурка Шируи', text: 'Фигурка Шируи из Kancolle', date: '29.04', price: '15000₽'},
-          {title: 'Фигурка Ремилия', text: 'Фигурка Ремилии из Touhou Project', date: '29.04', price: '12000₽'}
-        ].map(({title, text, date}) => (
+        {apps.map(({title, text, date}) => (
           <AppCard 
             title={title}
             text={text}
---
+++ src/variables.jsx
@@ -0,0 +1,9 @@
+export const apps = [
+    {title: 'Футболка Кага', text: 'Футболка с персонажем Кага из Kancolle', date: '27.04', price: '1500₽'}, 
+    {title: 'Кепка Эллиса', text: 'Кепка Эллиса из Left 4 Dead 2', date: '28.04', price: '975₽'}, 
+    {title: 'Фигурка Шируи', text: 'Фигурка Шируи из Kancolle', date: '29.04', price: '15000₽'},
+    {title: 'Фигурка Ремилия', text: 'Фигурка Ремилии из Touhou Project', date: '29.04', price: '12000₽'},
+    {title: 'Фигурка Ремилия', text: 'Фигурка Ремилии из Touhou Project', date: '29.04', price: '12000₽'},
+    {title: 'Фигурка Ремилия', text: 'Фигурка Ремилии из Touhou Project', date: '29.04', price: '12000₽'},
+    {title: 'Фигурка Ремилия', text: 'Фигурка Ремилии из Touhou Project', date: '29.04', price: '12000₽'}
+]
